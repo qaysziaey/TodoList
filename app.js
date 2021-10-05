9e4d278710d8050b83e9609820fe054c0b728f21
qaysziaey <techcorner95@gmail.com>
2021-10-05 14:45:59 +0200

    html, scss, js files has been added

---
+++ app.js
@@ -0,0 +1,36 @@
+const addItem = document.getElementById("add-btn");
+const deleteItem = document.getElementById("delete-btn");
+const listItem = document.getElementById("demo");
+
+addItem.addEventListener("click", AddItem);
+function AddItem() {
+  // Get the Userinput
+  let getInput = document.getElementById("user-input").value;
+  let list = document.createElement("li");
+
+  // Attach the Userinput date to the List Items
+  list.appendChild(document.createTextNode(getInput));
+  let addedItem = listItem.appendChild(list);
+
+  // Show up the message box
+  let showMsgBox = (document.querySelector(".message-box").style.visibility =
+    "visible");
+  let msgSuccess = (document.querySelector(".message-box p").textContent =
+    "Task has been added.");
+
+  if (addedItem && getInput) {
+    showMsgBox;
+    msgSuccess;
+  } else {
+    showMsgBox.style.visibility = "hidden";
+  }
+}
+
+// Erase the given input Data in Input field
+const eraseInput = document.getElementById("x-btn");
+
+eraseInput.addEventListener("click", EraseInput);
+function EraseInput() {
+  let getInput = (document.getElementById("user-input").value = "");
+}
+
