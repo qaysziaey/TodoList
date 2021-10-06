af0e13a4283f321a7788d1463c99b334ae8b27f6
qaysziaey <techcorner95@gmail.com>
2021-10-06 12:42:32 +0200

    changes in js code

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 const addItem = document.getElementById("add-btn");
 const deleteItem = document.getElementById("delete-btn");
-const listItem = document.getElementById("demo");
+const listItem = document.getElementById("task-item .title");
+
 
 addItem.addEventListener("click", AddItem);
 function AddItem() {
   // Get the Userinput
   let getInput = document.getElementById("user-input").value;
+  let taskArray = ["bla bla bla", 1234];
+
+  let newEls = taskArray.push(getInput)
+
+  console.log(newEls);
+
   let list = document.createElement("li");
+  
+
 
   // Attach the Userinput date to the List Items
   list.appendChild(document.createTextNode(getInput));
   let addedItem = listItem.appendChild(list);
 
-  // Show up the message box
-  let showMsgBox = (document.querySelector(".message-box").style.visibility =
-    "visible");
-  let msgSuccess = (document.querySelector(".message-box p").textContent =
-    "Task has been added.");
+  // Show the message box
+  let notificationMsg = document.createElement("p");
+  let showMsgBox = (document.querySelector(".message-box").style.visibility = "visible");
+  let msgSuccess = (document.querySelector(".message-box").textContent = "Task has been added.");
+  notificationMsg.appendChild(msgSuccess);
+
 
   if (addedItem && getInput) {
     showMsgBox
@@ -24,13 +34,17 @@ function AddItem() {
   } else {
     showMsgBox.style.visibility = "hidden";
   }
+
 }
 
-// Erase the given input Data in Input field
-const eraseInput = document.getElementById("x-btn");
 
-eraseInput.addEventListener("click", EraseInput);
+
+
+// Erase the given input Data in Input field
+const removeItem = document.getElementById("delete-btn");
+
+removeItem.addEventListener("click", EraseInput);
 function EraseInput() {
-  let getInput = (document.getElementById("user-input").value = "");
+  let getInput = document.getElementById("user-input").value;
 }
 
